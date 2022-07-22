63d35c862b9033fb0dd0b65440f182402ec1a5d1
Atulkumar Patil <atulp9602@gmail.com>
2022-07-23 00:12:59 +0530

    css applied

--- src/App.js
+++ src/App.js
@@ -16,17 +16,17 @@ function App() {
                 <Routes>
                   <Route exact path="/" element={<h1>Home Page</h1>} />
                   <Route exact path="page1" element={<Page1 />} />
-                  <Route exact path="page2" element={<Page2 />} />
+                  <Route exact path="page2" element={<Page2 />} />  
                   <Route exact path="page3" element={<Page3 />} />
                 </Routes>
 
             <div className="list">
-                <ol>
+                <ul className = "navs">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="page1">Page1</Link></li>
                     <li><Link to="page2">Page2</Link></li>
                     <li><Link to="page3">Page3</Link></li>
-                </ol>
+                </ul>
             </div>
         </Router>
     </div>
